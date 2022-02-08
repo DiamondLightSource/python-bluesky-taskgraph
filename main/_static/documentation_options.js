80dfbcc6f212f0717497cd36f1c14a681b53f2df
WareJosephB <WareJosephB@users.noreply.github.com>
2022-02-08 10:57:48 +0000

    deploy: 9e3c19660a93c4ed43641e7733f6de1650dd30bd

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.pre+22.g7792030',
+    VERSION: '0.0.1.pre+24.g9e3c196',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
